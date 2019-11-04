7d72559d288a7ccfaea617ccd33665559f46d686
peter liu <peterlxb@gmail.com>
2019-11-04 08:23:20 +0800

    channels/

---
+++ goByExample/channel-synchronization/channel-synchronization.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func work(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go work(done)
+	<-done
+}
